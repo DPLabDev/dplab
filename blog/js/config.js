e45e19c7ba314c644ab3f00894b3febb6c497ab5
Stefano Di Piazza <s.dipiazza@tiscali.it>
2018-05-17 20:04:30 +0200

    fix blo settings 2

--- blog/js/config.js
+++ blog/js/config.js
@@ -16,7 +16,7 @@ $(function() {
 
     // Navigation items
     siteNavItems: [
-      { name: 'Github', href: 'https://github.com/DPLabDev', newWindow: false},
+      { name: 'Github', href: 'https://github.com/DPLabDev/dplab', newWindow: false},
       { name: 'About'}
     ],
 
